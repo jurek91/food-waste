3a34d308bae0919d18295a92a28b14d768b792b0
Jurek Osada <j.c.osada@gmail.com>
2021-03-23 14:34:50 +1100

    created db and ran migration, made basic validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: {minimum: 5}
+  validates :first_name, presence: :true
+  validates :email, confirmation: true
+
+
 end
